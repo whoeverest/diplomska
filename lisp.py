d49cf2a52d45633c2dfa8d7a7624fd20d88ee791
Andrej T <andrejtrajchevski@gmail.com>
2016-07-08 12:01:03 +0200

    more elaborate calculator example

--- lisp.py
+++ lisp.py
@@ -1,20 +1,38 @@
 # This code:
 
-code = "(+ 2 (- 20 10) (+ 4 5 6))"
+#            a1        a2        a3
+code = "(+ 2 (- 20 10) (+ 4 5 6) (+ 1 2))"
 
 # Should produce:
 
 """"
+(eval 2)
 push 2
+
+(eval (- 20 10))
 push 20
 push 10
 subtract
+
+(eval (+ 4 5 6)) === (eval (+ (+ 4 5) 6)
 push 4
 push 5
 add
 push 6
 add
+
+(eval (+ 1 2))
+push 1
+push 2
 add
+
+(r1 = (a2 + a3))
+add
+
+(r2 = (a1 + r1))
+add
+
+(2 + r2)
 add
 """
 
